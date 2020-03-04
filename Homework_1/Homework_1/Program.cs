14631b8cc40c26c35e0da8a5bb633a92e769926c
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-03-04 13:05:55 +0100

    Add files via upload

---
+++ Homework_1/Homework_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor  = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+            Console.ReadLine();
+        }
+    }
+}
