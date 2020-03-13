001fcf2c6f5c4dd92658ed3a4c6a692e7d3932f6
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-03-13 23:34:13 +0100

    Update Program.cs

--- Homework_03/Homework_03/Program.cs
+++ Homework_03/Homework_03/Program.cs
@@ -96,31 +96,8 @@ namespace Homework_03
             }
             Console.ReadLine();
 
-            //string[] names = new string[10];
-            //var index = 0;
-            //Console.WriteLine("Please enter a name:");
-            //while (true)
-            //{
-            //    string userName = Console.ReadLine();
-            //    if (userName.Length > 1)
-            //    {
-            //        names[index] = userName;
-            //        index++;
-            //    }
-            //    Console.WriteLine("Would you like to enter another name? Y/N?");
-            //    if (userName.ToLower() == "n")
-            //    {
-
-            //        Console.WriteLine("\nThe entered names are: ");
-            //        foreach (var name in names)
-            //        {
-            //            Console.Write(name + " ");
-            //        }
-            //        break;
-            //    }
-            //}
-            //Console.ReadLine();
-
+            
+            
            #endregion
         }
     }
