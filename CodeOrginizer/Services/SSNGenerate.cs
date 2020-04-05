d4c27dcc498a2451118f294c63c79006cb3837c2
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-04-05 16:04:20 +0200

    Add files via upload

---
+++ CodeOrginizer/Services/SSNGenerate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonServices
+{
+    public class SSNGenerate
+    {
+        public static long GenerateSSN()
+        {
+            Random rand = new Random();
+            return rand.Next(1000000, 9999999);
+        }
+    }
+}
