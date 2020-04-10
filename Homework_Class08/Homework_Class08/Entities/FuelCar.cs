d8004280e978c6cb952167853f61d8a3dc697c76
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-04-10 20:23:09 +0200

    Update FuelCar.cs

--- Homework_Class08/Homework_Class08/Entities/FuelCar.cs
+++ Homework_Class08/Homework_Class08/Entities/FuelCar.cs
@@ -12,14 +12,17 @@ namespace Homework_Class08.Entities
 
 
         public int Refuel(int fuel)
-        {
-
+        {          
             if(fuel > FuelCapacity)
             {
                 Console.WriteLine($"Can't refuel more than {FuelCapacity} litres");
             }
-            Console.WriteLine($"Tank has been fueled with {fuel} l.");
-            return fuel;
+            else
+            {
+                Console.WriteLine($"Tank has been fueled with {fuel} l.");
+            }
+            
+            return fuel
 
         }
     }
