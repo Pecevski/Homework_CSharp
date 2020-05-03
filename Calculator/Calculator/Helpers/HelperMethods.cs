f55d157d7438aaad272ca5b33d81a2c00ae97e62
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-05-04 01:01:37 +0200

    Class Library
    Calculator

---
+++ Calculator/Calculator/Helpers/HelperMethods.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Helpers
+{
+    internal class HelperMethods
+    {
+        internal static decimal CalculateAverage(List<int> numbers)
+        {
+
+            int sum = 0;
+            numbers.ForEach(number => sum += number);
+            return Convert.ToDecimal(sum) / Convert.ToDecimal(numbers.Count);
+        }
+
+        internal static int FindMaxNumber(List<int> numbers)
+        {
+            int  max = 0;
+            foreach (var number in numbers)
+            {
+                if(number > max)
+                {
+                    max = number;
+                }              
+            }
+            return max;
+        }
+
+        internal static int FindMinNumber(List<int> numbers)
+        {
+            int min = FindMaxNumber(numbers);
+            foreach (var number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            return min;
+        }
+
+        internal static int MultiplyAllNumbers(List<int> numbers)
+        {
+            int multiplyAll = 1;
+            foreach (var number in numbers)
+            {
+                multiplyAll *= number;
+            }
+            return multiplyAll;
+        }
+
+        internal static int SumAllNumbers(List<int> numbers)
+        {
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ Calculator/Calculator/Models/Calc.cs
@@ -0,0 +1,36 @@
+﻿using Calculator.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Models
+{
+    public static class Calc
+    {
+        public static decimal Avg(List<int> numbers)
+        {
+            return HelperMethods.CalculateAverage(numbers);
+        }
+
+        public static int Max(List<int> numbers)
+        {
+            return HelperMethods.FindMaxNumber(numbers);
+        }
+
+        public static int Min(List<int> numbers)
+        {
+            return HelperMethods.FindMinNumber(numbers);
+        }
+
+        public static int Multiply(List<int> numbers)
+        {
+            return HelperMethods.MultiplyAllNumbers(numbers);
+        }
+
+        public static int SumAll(List<int> numbers)
+        {
+            return HelperMethods.SumAllNumbers(numbers);
+        }
+
+    }
+}
