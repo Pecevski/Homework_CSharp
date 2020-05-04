0c2c2d338610332953345db029b260815aa85031
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-05-04 23:34:37 +0200

    AcademyApp
    Refactored

---
+++ Class7FullStructure/AcademyApp/Helpers/ParticipantHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using AcademyApp.Entities;
+using AcademyApp.Enums;
+
+namespace AcademyApp.Helpers
+{
+    public class ParticipantHelper
+    {
+        // First parameter changed from array to List
+        public static void FindParticipantByRole(List<Participant> participants, AcademyRole role)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (var participant in participants)
+            {
+                if (participant.Role == role)
+                    participant.PrintFullName();
+            }
+        }
+
+    }
+}
