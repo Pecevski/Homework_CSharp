f03f658e7337f071094faa5f42e131fd32e5e51b
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-05-15 02:41:14 +0200

    Add files via upload

---
+++ ExceptionHandling/FakeFacebook/Logger/AdminExceptionLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FakeFacebook.Logger
+{
+    public class AdminExceptionLogger
+    {
+        private string FilePath { get; set; } = @"C:\Users\Dell\source\repos\ExceptionHandling\AdminExceptionLogger.txt";
+
+        public void AdminLogException(Exception ex)
+        {           
+            StreamWriter sw = new StreamWriter(FilePath);
+
+            sw.WriteLine($"{ex.GetType().Name} {ex.Message} {ex.StackTrace}");
+
+            sw.Close();
+        }
+    }
+}
---
+++ ExceptionHandling/FakeFacebook/Logger/UserExceptionLogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FakeFacebook.Logger
+{
+    public class UserExceptionLogger
+    {
+        private string FilePath { get; set; } = @"C:\Users\Dell\source\repos\ExceptionHandling\ExeptionLogger.txt";
+
+        public void LogException(Exception ex)
+        {
+            //open connection to file
+            StreamWriter sw = new StreamWriter(FilePath);
+
+            //write in file
+            sw.WriteLine($"{ex.GetType().Name} {ex.Message} {ex.StackTrace}");
+
+            //close connection to file
+            sw.Close();
+        }
+    }
+}
