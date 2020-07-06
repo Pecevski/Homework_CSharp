99ff5dad348fb90ada2da02581343badd94c887d
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-07-06 15:51:01 +0200

    Serializing_Deserializing

---
+++ Exercise_SD/Dog_JSON/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dog_JSON
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public Dog(string name, int age, string color)
+        {
+            Name = name;
+            Age = age;
+            Color = color;
+        }
+    }
+}
